9cd4f1df73ddc55df603c127cb078265903a52d7
Sten Kiissel <sten.kiissel@voco.ee>
2024-04-01 12:24:04 +0300

    Add player and it's game logic

--- game.py
+++ game.py
@@ -3,30 +3,25 @@
 # Import and initialize the pygame library
 import pygame
 from settings import Settings
+from player import Player
+import game_functions as gf
 
 def run_game():
     pygame.init()
     gm_settings = Settings()
 
-
-
     # Set up the drawing window
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
 
+    # Instantiate player
+    player = Player(screen)
+
+
     # Run untill the user asks to quit
-    running = True
-    while running:
-        screen.fill(gm_settings.bg_color)
-        # Did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            
-        # Flip the display
-        pygame.display.flip()
-    
-    # Done! Time to quit.
-    pygame.quit()
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.update_screen(gm_settings, screen, player)
 
 run_game()
